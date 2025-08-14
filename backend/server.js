1a8126c13713ac6e1dccaf0b6ec96992ff3f22b1
Kamlesh Gehlot <148068348+kamlesh9685@users.noreply.github.com>
2025-08-14 17:48:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ app.use(express.json())
 
 app.use(cors({
   origin: ['https://docline-frontend.onrender.com'], // yaha frontend live URL
-  credentials: true
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true 
 }))
 
 // API routes
